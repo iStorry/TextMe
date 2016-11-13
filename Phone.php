6b627616aa5e46e6e8397c5180dcbfc8595fb029
Jatin <jatinx.lol@gmail.com>
2016-11-14 00:30:53 +0530

    Update Phone.php

--- Phone.php
+++ Phone.php
@@ -6,7 +6,7 @@
 
 	 if (isset($_REQUEST["verification_token"], $_REQUEST["email"]))
 	 {
-	 	$data = explode(":", file_get_contents("http://istorry.com/TextMe/save/".$_REQUEST["email"].".txt"));
+	 	$data = explode(":", file_get_contents("http://example.com/TextMe/save/".$_REQUEST["email"].".txt"));
 	 	$ne->signupWithToken($_REQUEST["verification_token"], $data[2], $data[0], $data[1], $data[3]);
 	 	echo "Username : " . $data[2] . "<br>";
 	 	echo "Email : " . $data[0] . "<br>";
